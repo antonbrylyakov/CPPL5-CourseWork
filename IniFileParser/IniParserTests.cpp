1381e41333061b40fd81f9a1de3c911e1cf549c2
Антон Брыляков <yoll@rambler.ru>
2023-01-13 01:42:46 -0800

    Тесты на ненахождение данных

--- IniFileParser/IniParserTests.cpp
+++ IniFileParser/IniParserTests.cpp
@@ -28,3 +28,53 @@ TEST_CASE("Нахождение параметра", "[IniParser]") {
 		CHECK(value == 1.2);
 	}
 }
+
+TEST_CASE("Ненахождение параметра", "[IniParser]") {
+
+	SECTION("Ненахождение секции")
+	{
+		std::unique_ptr<std::istream> stream = std::make_unique<std::istringstream>("[section1]\r\nparam1=value1");
+		IniParser parser(std::move(stream));
+		try
+		{
+			parser.getValue<std::string>("section2.param1");
+		}
+		catch (ReadParamError& e)
+		{
+			std::string s(e.what());
+			CHECK(s.find("Допустимые секции: 'section1'"));
+		}
+
+		SECTION("Ненахождение параметра")
+		{
+			std::unique_ptr<std::istream> stream = std::make_unique<std::istringstream>("[section1]\r\nparam1=value1");
+			IniParser parser(std::move(stream));
+			try
+			{
+				parser.getValue<std::string>("section1.param2");
+			}
+			catch (ReadParamError& e)
+			{
+				std::string s(e.what());
+				CHECK(s.find("Допустимые параметры: 'param1'"));
+			}
+		}
+	}
+}
+
+TEST_CASE("Невалидные значения", "[IniParser]") {
+
+	SECTION("int")
+	{
+		std::unique_ptr<std::istream> stream = std::make_unique<std::istringstream>("[section1]\r\nparam1=value1");
+		IniParser parser(std::move(stream));
+		CHECK_THROWS_AS(parser.getValue<int>("section1.param1"), ReadParamError);
+	}
+
+	SECTION("double")
+	{
+		std::unique_ptr<std::istream> stream = std::make_unique<std::istringstream>("[section1]\r\nparam1=value1");
+		IniParser parser(std::move(stream));
+		CHECK_THROWS_AS(parser.getValue<double>("section1.param1"), ReadParamError);
+	}
+}
